48c7899eb115e0a7f5368e04803fe51dc60ce46e
Heretinih <heretinih@gmail.com>
2026-01-18 15:11:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const app = express();
 
 app.use(express.json());
 
+// LOG EVERY REQUEST (CRITICAL DEBUG)
+app.use((req, res, next) => {
+  console.log("=== INCOMING REQUEST ===");
+  console.log("Method:", req.method);
+  console.log("Headers:", req.headers);
+  console.log("Body:", JSON.stringify(req.body, null, 2));
+  console.log("========================");
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Render service is running");
 });
 
 app.post("/", (req, res) => {
-  console.log("Incoming webhook:", req.body);
   res.status(200).send("OK");
 });
 
